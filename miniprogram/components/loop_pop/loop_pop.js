d981c47c2dbac84a841fa62aba987dfd0b4a0c32
jqxiong <jqxiong@trip.com>
2020-08-28 18:39:20 +0800

    update

--- miniprogram/components/loop_pop/loop_pop.js
+++ miniprogram/components/loop_pop/loop_pop.js
@@ -4,15 +4,15 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    isupdate: {
-      type: Boolean,
-      default: false,
-      observer: function(newVal, oldVal) {
-        if (newVal != oldVal) {
-          this.queryInviteList()
-        }
-      }
-    }
+    // isupdate: {
+    //   type: Boolean,
+    //   default: false,
+    //   observer: function(newVal, oldVal) {
+    //     if (newVal != oldVal) {
+    //       this.queryInviteList()
+    //     }
+    //   }
+    // }
   },
 
   /**
@@ -22,8 +22,9 @@ Component({
     userList: []
   },
 
-  attached: function() {
-    this.queryInviteList();
+  attached: async function() {
+    await this.queryInviteList();
+    this.initDbWatcher()
   },
   /**
    * 组件的方法列表
@@ -38,6 +39,27 @@ Component({
       this.setData({
         userList: data
       })
+    },
+    initDbWatcher: async function() {
+ 
+      // 监听数据变化推送
+      const db = wx.cloud.database({
+        env: 'dev-615384'
+      });
+
+      const _ = db.command
+      const rst = await db.collection('user_invite').where({
+        isAccepted: _.eq(true)
+      }).get()
+      console.log(rst)
+      // db.collection('user_invite').where({}).watch({
+      //   onChange: function(rst) {
+      //     console.log(rst)
+      //   },
+      //   onError: function(err) {
+      //     console.log(err)
+      //   }
+      // })
     }
   }
 })
